b434ffc77d4475ebd99765558100871d309bfc4f
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-05-08 21:17:43 +0800

    [Other]

--- module/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import pathlib
-
-__all__ = []
-
-for path in pathlib.Path(os.path.dirname(__file__)).iterdir():
-    if (path.is_dir() or path.name.endswith(".py")) and not path.name.startswith("_"):
-        __all__.append(path.stem)
