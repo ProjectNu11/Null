13b49d76df8a4e8eefe50cfcdcc33cb8c50d7c62
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-08-09 12:50:41 +0800

    :no_entry: Banned anonymous user

--- library/depend/blacklist.py
+++ library/depend/blacklist.py
@@ -13,17 +13,31 @@ class Blacklist:
     @staticmethod
     def check(
         on_failure: MessageChain = None,
+        allow_anonymous: bool = False,
+        on_anonymous: MessageChain | None = MessageChain("本服务不允许匿名用户使用"),
     ) -> Depend:
         """
         Check switch.
 
         :param on_failure: Message chain to send when switch is off.
+        :param allow_anonymous: Allow anonymous user to use this module.
+        :param on_anonymous: Message chain to send when anonymous user use this module.
         :return: Depend decorator.
         """
 
         async def blacklist_check(event: MessageEvent) -> NoReturn:
             field = event.sender.group.id if isinstance(event, GroupMessage) else 0
             target = event.sender.id
+            if target == 80000000 and not allow_anonymous:
+                if on_failure:
+                    await Ariadne.current().send_message(
+                        event.sender.group
+                        if isinstance(event, GroupMessage)
+                        else event.sender,
+                        on_anonymous.as_sendable(),
+                    )
+                raise ExecutionStop
+
             if any(
                 [
                     await blacklist.check(field=-1, target=target),
